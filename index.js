aaa0228d42c1a1d761146ca828456184ca9ab516
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-09-19 18:42:16 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(i) {
+    return i += 1;
+}
+
+function decrement(i) {
+    return i -= 1;
+}
+
+function makeInt(i) {
+    return parseInt(i, 10);
+}
+
+function preserveDecimal(i) {
+    return parseFloat(i);
+}
